594ffcde47deca418f51fb9650a7481be090a1f8
FedeClaudi <federicoclaudi@protonmail.com>
2020-11-05 15:06:46 +0000

    bump

--- show_colors.py
+++ show_colors.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 from myterial.lookup import colors
 
 
-f, axarr = plt.subplots(ncols=5, nrows=10, figsize=(10, 10))
+f, axarr = plt.subplots(ncols=5, nrows=20, figsize=(10, 20))
 axarr = axarr.flatten()
 
 for ax, (name, hex) in zip(axarr, colors.items()):
     ax.scatter(0, 0, color=hex, lw=1, edgecolors='k', s=2000)
-    ax.axis('off')
     ax.set(title=name)
+
+for ax in axarr:
+    ax.axis('off')
 
 f.tight_layout()
 plt.show()
