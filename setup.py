7655e1371b2fba2a119cc54f56d18d221975900b
FedeClaudi <federicoclaudi@protonmail.com>
2021-03-17 08:51:50 +0000

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name="myterial",
-    version="1.0",
+    version="1.1",
     description="Easy acces to material colors hex codes",
     long_description=long_description,
     long_description_content_type="text/markdown",
